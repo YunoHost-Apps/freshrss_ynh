aed810e72550b8ef13bd47996bbb767976c65d56
plopoyop <clement@iMac-de-Clement.local>
2014-07-23 16:56:39 +0200

    corrections http_auth

--- conf/config.php
+++ conf/config.php
@@ -9,7 +9,7 @@
     'default_user' => 'yunoadminuser',
     'allow_anonymous' => false,
     'allow_anonymous_refresh' => false,
-    'auth_type' => 'http',
+    'auth_type' => 'http_auth',
     'api_enabled' => true,
     'unsafe_autologin_enabled' => false,
   ),
