b2b840a3191ef8082dd0b0555a68058a953c6abf
yalh76 <yalh@yahoo.com>
2019-07-02 00:38:57 +0200

    Apply example_ynh

--- scripts/_common.sh
+++ scripts/_common.sh
@@ -1,17 +1,8 @@
-#
-# Common variables
-#
-APPNAME="freshrss"
-# FreshRSS version
-VERSION="1.14.1"
+#!/bin/bash
 
-# FreshRSS complete tarball checksum
-FRESHRSS_SOURCE_SHA256="fd5ce6906021282e12cd6346844d21b82b70ac12d08834079a46b8722bfb0cce"
-
-# Remote URL to fetch FreshRSS source tarball
-FRESHRSS_SOURCE_URL="https://github.com/FreshRSS/FreshRSS/archive/${VERSION}.tar.gz"
-
-PKGDIR=$(cd ../; pwd)
+#=================================================
+# COMMON VARIABLES
+#=================================================
 
 if [ "$(lsb_release --codename --short)" == "jessie" ]; then
 	pkg_dependencies="php5-gd"
@@ -19,25 +10,14 @@ else
 	pkg_dependencies="php-gd php-zip php-dom php-mbstring"
 fi
 
-#
-# Common helpers
-#
-# Download and extract FreshRSS sources to the given directory
-# usage: extract_freshrss DESTDIR
-extract_freshrss() {
-  local DESTDIR=$1
+#=================================================
+# PERSONAL HELPERS
+#=================================================
 
-  # retrieve and extract FreshRSS tarball
-  rc_tarball="${DESTDIR}/freshrss.tar.gz"
-  wget -q -O "$rc_tarball" "$FRESHRSS_SOURCE_URL" \
-    || ynh_die "Unable to download FreshRSS tarball"
-  echo "$FRESHRSS_SOURCE_SHA256 $rc_tarball" | sha256sum -c >/dev/null \
-    || ynh_die "Invalid checksum of downloaded tarball"
-  tar xf "$rc_tarball" -C "$DESTDIR" --strip-components 1 \
-    || ynh_die "Unable to extract FreshRss tarball"
-  sudo rm "$rc_tarball"
-}
+#=================================================
+# EXPERIMENTAL HELPERS
+#=================================================
 
-install_freshrss_dependencies() {
-    ynh_install_app_dependencies $pkg_dependencies
-}
+#=================================================
+# FUTURE OFFICIAL HELPERS
+#=================================================
