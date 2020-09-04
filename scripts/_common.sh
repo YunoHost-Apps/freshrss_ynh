9b713b904d73688d335fe3429727d92c945bca25
ericgaspar <junk.eg@free.fr>
2020-09-04 22:24:17 +0200

    php as variable

--- scripts/_common.sh
+++ scripts/_common.sh
@@ -4,11 +4,9 @@
 # COMMON VARIABLES
 #=================================================
 
-if [ "$(lsb_release --codename --short)" == "jessie" ]; then
-	pkg_dependencies="php5-gd"
-else
-	pkg_dependencies="php-gd php-zip php-dom php-mbstring php-gmp php-sqlite3 php-curl php-intl php-xml"
-fi
+pkg_dependencies="php${YNH_PHP_VERSION}-gd php-zip php${YNH_PHP_VERSION}-dom php${YNH_PHP_VERSION}-mbstring php${YNH_PHP_VERSION}-gmp php${YNH_PHP_VERSION}-sqlite3 php${YNH_PHP_VERSION}-curl php${YNH_PHP_VERSION}-intl php${YNH_PHP_VERSION}-xml"
+
+YNH_PHP_VERSION="7.3"
 
 #=================================================
 # PERSONAL HELPERS
