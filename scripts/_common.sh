e6b8ecfd663004086b7e7f2735537d1a51a6d17c
Jules Bertholet <jules.bertholet@gmail.com>
2021-04-17 12:39:02 -0400

    More tweaks to permissions

--- scripts/_common.sh
+++ scripts/_common.sh
@@ -14,6 +14,13 @@ extra_php_dependencies="php${YNH_PHP_VERSION}-gd php${YNH_PHP_VERSION}-zip php${
 # PERSONAL HELPERS
 #=================================================
 
+function set_permissions {
+	chown -R root:$app $final_path
+	chmod -R g=u,g-w,o-rwx $final_path
+	chown -R $app:$app $final_path/{data,extensions}
+	setfacl -n -R -m u:www-data:rx -m d:u:www-data:rx $final_path
+}
+
 #=================================================
 # EXPERIMENTAL HELPERS
 #=================================================
